77f4a2da53fa0857b1dd1aa9e0265a35a0048b67
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-11-04 01:31:37 +0700

    ✦ Update to per minute arbitrage

--- src/cron.ts
+++ src/cron.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
 
-cron.schedule("*/3 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
     // Chains
     const chainID = parseInt(process.env.CHAIN_ID);
 
@@ -26,8 +26,11 @@ cron.schedule("*/3 * * * *", async () => {
     const token0 = new Token(chainID, token0Address, token0Decimal);
     const token1 = new Token(chainID, token1Address, token1Decimal);
 
+    // Create the wallet
+    const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
+
     try {
-        await arbitrage(chainID, provider, token0, token1);
+        await arbitrage(token0, token1, wallet);
     } catch (e) {
         console.error("Failed to run arbitrage:", e);
     }
