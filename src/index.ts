50271a74c6ea77f1a2d22984ddcbb5c92e646dd0
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-11-03 22:17:38 +0700

    ✦ Create one-off script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import cron from "node-cron";
+import arbitrage from "./arbitrage";
+import { Token } from "@uniswap/sdk-core";
+import dotenv from "dotenv";
+import { ethers } from "ethers";
 
-cron.schedule("* * * * * *", () => {
-    console.log("[arbitrage] Run");
-});
+// Load environment variables
+dotenv.config();
+
+// Chains
+const chainID = parseInt(process.env.CHAIN_ID);
+
+// Initialize provider
+const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_URL);
+
+// Contract addresses
+const token0Address = process.env.TOKEN0_ADDRESS;
+const token0Decimal = parseInt(process.env.TOKEN0_DECIMAL);
+const token1Address = process.env.TOKEN1_ADDRESS;
+const token1Decimal = parseInt(process.env.TOKEN1_DECIMAL);
+
+// Create tokens
+const token0 = new Token(chainID, token0Address, token0Decimal);
+const token1 = new Token(chainID, token1Address, token1Decimal);
+
+arbitrage(chainID, provider, token0, token1);
